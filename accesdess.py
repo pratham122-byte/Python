592ec5d83fbbf2fb243473514e9bd1c1425925d9
Pratham Kamath <prathamkamath627@gmail.com>
2026-05-20 19:17:34 +0530

    Add files via upload

---
+++ accesdess.py
@@ -0,0 +1,5 @@
+num=[8,5,9,2,90,12,2]
+num.sort()
+print(num)
+num.sort(reverse=True)
+print(num)
---
+++ ad.py
@@ -0,0 +1,5 @@
+#add the two number
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+sum=a+b 
+print(sum)
