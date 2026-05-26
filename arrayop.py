abf0318743a0591475813f653c53bc760fe15ffc
Pratham Kamath <prathamkamath627@gmail.com>
2026-05-26 16:03:35 +0530

    Add files via upload

---
+++ arrayop.py
@@ -0,0 +1,26 @@
+import numpy as np
+first_array=np.array([1,2,3,4])
+second_array=np.array([5,6,7,8])
+thrid_array=np.array([12,3,4,45])
+result=first_array+second_array+thrid_array
+print("using + operrator",result)
+result1=np.add(first_array,second_array)
+print("using the add function",result1)
+#sub
+first_array=np.array([1,2,3,4])
+second_array=np.array([5,6,7,8])
+thrid_array=np.array([12,3,4,45])
+result=first_array-second_array-thrid_array
+print("using + operrator",result)
+#mul
+first_array=np.array([1,2,3,4])
+second_array=np.array([5,6,7,8])
+thrid_array=np.array([12,3,4,45])
+result=first_array*second_array*thrid_array
+print("using + operrator",result)
+#div
+first_array=np.array([1,2,3,4])
+second_array=np.array([5,6,7,8])
+thrid_array=np.array([12,3,4,45])
+result=first_array/second_array/thrid_array
+print("using + operrator",result)
