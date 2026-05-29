4213a39736757c02e46a7cbf0dd6ae463a5ed281
Pratham Kamath <prathamkamath627@gmail.com>
2026-05-29 22:44:59 +0530

    Add files via upload

---
+++ book.py
@@ -0,0 +1,6 @@
+def add_book(book_name, author):
+    print("book added")
+    print("Book Name:", book_name)
+    print("Author:", author)
+def issue_book(book_name):
+    print(f"Book '{book_name}' issued successfully.")
