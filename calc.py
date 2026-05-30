203e4f95551c784db92daa0dde0ca8bc15df442a
Pratham Kamath <prathamkamath627@gmail.com>
2026-05-30 23:04:46 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,27 @@
+def add(x,y):
+    return (x+y)
+def subtract(x,y):
+    return (x-y)
+def mul(x,y):
+    return (x*y)
+def div(x,y):
+    return (x/y)
+def triangle(x,y):
+    return (0.5*x*y)
+def circle(x):
+    return (2*3.14*x)
+def larger(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+def possitive(x):
+    if x>=0:
+        return True
+    else:
+        return False   
+def evenodd(x):
+    if x%2==0:
+        return "even"
+    else:
+        return "odd"
