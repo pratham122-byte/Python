92cdb19ce912894f053abade7bd6757673f2c656
Pratham Kamath <prathamkamath627@gmail.com>
2026-06-02 19:49:47 +0530

    Add files via upload

---
+++ dicgetmethod.py
@@ -0,0 +1,5 @@
+d={"coding":'good',"dancing":'bad',"reading":'average'}
+print(d.get("coding"))
+dictionary1={"name":"Alice","age":30,"city":"New York"} 
+print(dictionary1.keys())
+print(dictionary1.values())
