0ebbff1aaea288205d9a09bd7d2ea3d25bc6c796
Pratham Kamath <prathamkamath627@gmail.com>
2026-07-14 17:55:49 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,8 @@
+number={29,24,47,68,15,4,9}
+number1={29,34,716,35,4,1,4,5,77,8,9}
+print("original set:",number)
+number.add(55)
+print("after add",number)
+print("union",number|number1)
+print("intersection",number & number1)
+print("difference",number-number1)
