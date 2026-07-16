a8626b0e73cdd209ff06b20f645df2d9263e5299
Pratham Kamath <prathamkamath627@gmail.com>
2026-07-16 18:39:15 +0530

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([637,450,789,79,310])
+highest_price=np.max(arr)
+lowest_price=np.min(arr)
+closing_price=arr[-1]
+print("Highest Stock Price =",highest_price)
+print("Lowest Stock Price =",lowest_price)
+print("Closing Stock Price =",closing_price) 
